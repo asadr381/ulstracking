5109ecfc0ff15423c691a93a29461bdf53beb35f
asadr381 <101998038+asadr381@users.noreply.github.com>
2024-10-18 13:44:49 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes instead of Switch
 import './index.css';
 import App from './App';
+import ShipmentDetails from './shipment-details'; // Import your new component
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router> {/* Wrap your App with Router */}
+      <Routes> {/* Use Routes to render routes */}
+        <Route path="/" element={<App />} /> {/* Route for the main App */}
+        <Route path="/shipment-details" element={<ShipmentDetails />} /> {/* Route for ShipmentDetails */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
